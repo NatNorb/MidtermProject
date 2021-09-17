1fe9b855cdaedef6eb74fcfc1822e90e691882c8
NatNorb <natalia.n@op.pl>
2021-09-17 22:12:49 +0200

    <24 account

--- src/main/java/com/ironhack/MidtermProject/service/impl/AccountService.java
+++ src/main/java/com/ironhack/MidtermProject/service/impl/AccountService.java
@@ -2,8 +2,12 @@ package com.ironhack.MidtermProject.service.impl;
 
 import com.ironhack.MidtermProject.dao.accounts.Account;
 import com.ironhack.MidtermProject.dao.accounts.Checking;
+import com.ironhack.MidtermProject.dao.accounts.StudentChecking;
+import com.ironhack.MidtermProject.dao.users.AccountHolder;
 import com.ironhack.MidtermProject.repository.accounts.AccountRepository;
 import com.ironhack.MidtermProject.repository.accounts.CheckingRepository;
+import com.ironhack.MidtermProject.repository.accounts.StudentCheckingRepository;
+import com.ironhack.MidtermProject.repository.users.AccountHolderRepository;
 import com.ironhack.MidtermProject.service.interfaces.IAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,6 +22,34 @@ public class AccountService implements IAccountService {
 
     @Autowired
     public AccountRepository accountRepository;
+    @Autowired
+    public CheckingRepository checkingRepository;
+    @Autowired
+    public StudentCheckingRepository studentCheckingRepository;
+    @Autowired
+    public AccountHolderRepository accountHolderRepository;
+
+    public Account create(Checking checking) {
+        Optional<Checking> ch = checkingRepository.findById(checking.getId());
+
+        Optional<AccountHolder> accountHolder = accountHolderRepository.findById(checking.getAccountHolder().getAccHolderId());
+
+        if (ch.isEmpty()){
+            if (accountHolder.get().howOld() > 24){
+                Checking newChecking = new Checking(checking.getBalance(), checking.getPrimaryOwner(),
+                        checking.getSecondaryOwner(), checking.getAccountHolder());
+                return checkingRepository.save(newChecking);
+            } else{
+                StudentChecking newStudentChecking = new StudentChecking(checking.getBalance(), checking.getPrimaryOwner(),
+                        checking.getSecondaryOwner(), checking.getAccountHolder());
+                return studentCheckingRepository.save(newStudentChecking);
+            }
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The account id already exists in the system.");
+        }
+
+    }
+
 
     public void deposit(Long id, String owner, BigDecimal amount){
         Optional<Account> accDeposit = accountRepository.findById(id);
--- src/main/java/com/ironhack/MidtermProject/service/interfaces/ICheckingService.java
+++ src/main/java/com/ironhack/MidtermProject/service/interfaces/ICheckingService.java
@@ -1,9 +1,10 @@
 package com.ironhack.MidtermProject.service.interfaces;
 
+import com.ironhack.MidtermProject.dao.accounts.Account;
 import com.ironhack.MidtermProject.dao.accounts.Checking;
 
 public interface ICheckingService {
 
-    Checking createChecking(Checking checking);
+    Account createChecking(Checking checking);
     void modifyCheckingBalance(Long id, Checking checking);
 }
